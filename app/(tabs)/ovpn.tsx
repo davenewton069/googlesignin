5271f11ae401d3fcf8d5992be50c3810254fd133
davenewton069 <168262187+davenewton069@users.noreply.github.com>
2024-11-20 18:19:25 +0100

    Update ovpn.tsx

--- app/(tabs)/ovpn.tsx
+++ app/(tabs)/ovpn.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Alert, View } from 'react-native';
-import { GoogleSignin, statusCodes, GoogleSigninButton } from '@react-native-google-signin/google-signin';
+import { GoogleSignin, statusCodes, GoogleSigninButton, isSuccessResponse, isErrorWithCode } from '@react-native-google-signin/google-signin';
 import {GOOGLE_LOGIN_URL, GOOGLE_WEB_CLIENT_ID} from '@env';
 
 GoogleSignin.configure({
@@ -13,44 +13,34 @@ GoogleSignin.configure({
 
 const App: React.FC<{ navigation: any }> = ({ navigation }) => {
     const handleGoogleSignIn = async () => {
-        const url = ''//new URL(GOOGLE_LOGIN_URL);
-        try {
-            await GoogleSignin.hasPlayServices();
-            const userInfo = await GoogleSignin.signIn();
-            const token = userInfo;
-
-            console.log(token)
-
-            // Send idToken to the Django backend
-            const response = await fetch(url, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({ token: token }),
-            });
-
-            if (response.ok) {
-                const { access, refresh } = await response.json();
-                // Save tokens and navigate to the authenticated screen
-                console.log('Tokens:', { access, refresh });
-                Alert.alert('Success', 'Successfully signed in');
-                navigation.navigate('AppNavigator')
-            } else {
-                Alert.alert('Error', 'Failed to sign in with Google');
-            }
-        } catch (error: any) {
-            if (error.code === statusCodes.SIGN_IN_CANCELLED) {
-                Alert.alert('Cancelled', 'User cancelled the login process');
-            } else if (error.code === statusCodes.IN_PROGRESS) {
-                Alert.alert("Sign in in progress");
-            } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
-                Alert.alert("Play services not available");
-            } else {
-                Alert.alert("An error occurred", error.message);
-                console.log(error)
-            }
-        }
+        // const url = ''//new URL(GOOGLE_LOGIN_URL);
+         try {
+             await GoogleSignin.hasPlayServices();
+             const response = await GoogleSignin.signIn();
+             if (isSuccessResponse(response)) {
+               console.log( response.data );
+               navigation.navigate("AppNavigator")
+             } else {
+               // sign in was cancelled by user
+             }
+           } catch (error: any) {
+             if (isErrorWithCode(error)) {
+               switch (error.code) {
+                 case statusCodes.IN_PROGRESS:
+                   // operation (eg. sign in) already in progress
+                   break;
+                 case statusCodes.PLAY_SERVICES_NOT_AVAILABLE:
+                   // Android only, play services not available or outdated
+                   break;
+                 default:
+                   Alert.alert("An error occurred", error.message);
+                 // some other error happened
+               }
+             } else {
+                 Alert.alert("An error occurred", error);
+               // an error that's not related to google sign in occurred
+             }
+         }
     };
 
     return (
